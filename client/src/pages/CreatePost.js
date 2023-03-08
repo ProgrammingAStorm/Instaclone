4398e48a12b05ab93b315103b67d5e0ca7a0888b
Mark Pavel <markpavel02@gmail.com>
2023-03-08 13:52:57 -0500

    Added CreatePost Input Validtion

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -7,12 +7,34 @@ import Button from 'react-bootstrap/Button';
 const CreatePost = (props) => {
     const [file, setFile] = useState('');
     const [caption, setCaption] = useState('');
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = async (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(true);
+            return;
+        }
+
+
+        const formData = new FormData();
+
+        formData.append("image", file)
+        formData.append("imageCaption", caption)
+
+        await fetch("/api/post", {
+            method: "POST",
+            body: formData
+        });
+    };
 
     return (
         <Container>
-            <Form>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Group
-                    className=' m-1'
+                    className=' m-2'
                 >
                     <Form.Label>Upload Image</Form.Label>
                     <Form.Control
@@ -20,40 +42,40 @@ const CreatePost = (props) => {
                         required
                         name='image'
                         onChange={e => {
-                            setFile(e.target.files[0])
+                            const file = e.target.files[0];
+
+                            if (file.size > 2000000) {
+                                setValidated(true);
+                                e.target.value = '';
+                                return;
+                            }
+
+                            setFile(file);
                         }}
                     />
+
+                    <Form.Text muted>
+                        Your image cannot be larger than 2MB.
+                    </Form.Text>
                 </Form.Group>
 
                 <Form.Group
-                    className='d-flex flex-column m-1'
+                    className='d-flex flex-column m-2'
                 >
                     <Form.Label>Add Caption</Form.Label>
                     <Form.Control
                         as="textarea"
                         rows={5}
-                        required
                         name='imageCaption'
+                        required
                         onChange={e => {
                             setCaption(e.target.value)
                         }}
                     />
+
                     <Button
                         className='m-2 align-self-center'
                         type='submit'
-                        onClick={async e => {
-                            e.preventDefault()
-
-                            const formData = new FormData();
-
-                            formData.append("image", file)
-                            formData.append("imageCaption", caption)
-
-                            fetch("/api/post", {
-                                method: "POST",
-                                body: formData
-                            });
-                        }}
                     >
                         Submit
                     </Button>
